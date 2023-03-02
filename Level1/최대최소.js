1cf553d5b03928575705e8d56bcede1ac9ef6145
sujeong816 <sujeong1517@naver.com>
2023-03-03 00:28:47 +0900

    최대공약수와 최소공배수

---
+++ Level1/최대최소.js
@@ -0,0 +1,9 @@
+function solution(n, m) {
+    var answer = []; let max = 0; /**최대공약수*/ let min = 0; //최소공배수 
+    for(let i=1; i<=n*m; i++) { 
+        if(n%i==0 && m%i==0) { max = i; }
+    }
+    min = n*m / max;
+    answer.push(max); answer.push(min);
+    return answer;
+}

e91b98028906ca01a14fdc1a838c04b0bdc1512b
sujeong816 <sujeong1517@naver.com>
2023-03-15 15:31:24 +0900

    chap02 5. 모듈 export & import & export default

---
+++ node1/chap02/test_math.js
@@ -0,0 +1,14 @@
+import { average, sum } from './math.js'
+
+console.log(average(3,4,5,6));
+console.log(sum(3,4,5,6));
+
+console.log("----------------------")
+
+import { average2, PI } from './math.js';
+console.log(average2(3,4,5,6));
+console.log(PI);
+
+import M from './math.js'
+console.log(M.average2(3,4,5,6));
+console.log(M.PI);

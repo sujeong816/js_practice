94ce7f02560c0508c52f9753506e7af356ea4139
sujeong816 <sujeong1517@naver.com>
2023-03-22 18:56:23 +0900

    프론트엔드 프레임워크 02 react ch01 첫 프로젝트

---
+++ school/framework/ch02/ch01_5/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  console.log("App() 함수 호출됨");
+  return (
+    <div>
+      {" "}
+      <h1>안녕하세요 리액트</h1>{" "}
+    </div>
+  );
+}
+export default App;

f159eb19bcb8e21b20b6411d4c5f8ff3c7efb516
sujeong816 <sujeong1517@naver.com>
2023-03-23 17:23:09 +0900

    M: modified file

--- backjoon/1000.js
+++ backjoon/1000.js
@@ -1,5 +1,7 @@
-/* eslint-disable no-undef */
-let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-var a = parseInt(input[0]);
-var b = parseInt(input[1]);
-console.log(a+b);
+import { readFileSync } from 'fs';
+const inputData = readFileSync(0, 'utf8').toString().split(' ');
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A+B);
--- school/framework/ch02/ch01_5/src/App.tsx
+++ school/framework/ch02/ch01_5/src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import * as Data from "./data";
+
 function App() {
   console.log("App() 함수 호출됨");
   return (
     <div>
-      {" "}
-      <h1>안녕하세요 리액트</h1>{" "}
+      <h1>안녕하세요 리액트</h1>
+      <p>이름: {Data.randomName()}</p>
+      <p>직업: {Data.randomJobTitle()}</p>
+      <p>날짜:{Data.randomFullDateString()}</p>
+      <img src={Data.picsumUrl(200, 100)} />
+      <img src={Data.picsumUrl(300, 200)} />
     </div>
   );
 }

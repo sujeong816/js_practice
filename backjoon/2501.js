b09d209b675543908fb91d9e24474e53e2788d0e
sujeong816 <sujeong1517@naver.com>
2023-03-24 11:06:21 +0900

    setting

--- backjoon/2501.js
+++ backjoon/2501.js
@@ -1,5 +1,7 @@
-import { readFileSync } from 'fs';
-let inputData = readFileSync('/dev/stdin').toString().split(' ');
+import fs from "fs";
+//const fs = require('fs');
+// let inputData = fs.readFileSync('/dev/stdin').toString().split(' ');
+let inputData = fs.readFileSync('./index.txt').toString().split(' ');
 
 const n = parseInt(inputData[0]);
 const index = parseInt(inputData[1])-1; 
@@ -8,5 +10,4 @@ let arr = [];
 for(let i=1; i<=n; i++) {
     if(n%i==0) { arr.push(i); }
 }
-
-console.log(arr[index]);
+arr.length <= index ? console.log(0) : console.log(arr[index]);

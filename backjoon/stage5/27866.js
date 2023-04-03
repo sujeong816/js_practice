4671ae7b0ebd8c5cccca923ac28c5259a91a363c
sujeong816 <sujeong1517@naver.com>
2023-04-03 21:02:08 +0900

    백준 27866번 문자와 문자열

---
+++ backjoon/stage5/27866.js
@@ -0,0 +1,5 @@
+import fs from "fs";
+//const fs = require('fs');
+let inputData = fs.readFileSync("./index.txt").toString().trim().split("\n");
+// let inputData = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+console.log(inputData[0].charAt(inputData[1]-1))

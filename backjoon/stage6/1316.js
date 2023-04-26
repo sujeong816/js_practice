6ebe0b8c96c69e493127116286c08dceef95aa60
sujeong816 <sujeong1517@naver.com>
2023-04-26 22:57:12 +0900

    백준 1316번 그룹 단어 체커

--- backjoon/stage6/1316.js
+++ backjoon/stage6/1316.js
@@ -2,16 +2,24 @@ import fs from "fs";
 //const fs = require('fs');
 let inputData = fs.readFileSync("./index.txt").toString().trim().split("\n");
 // let inputData = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-let count = 0;
-for(let i=1; i<inputData.length; i++) {
-    let arr = inputData[i].split(' ')
-    if(arr.length == 1 || arr.length == 2) {
-        count++
-    } else {
-        for(let j=0; j<arr.length; j++) {
-            let str = arr[j];
-            
-        }
+let arr = []
+
+for (let i = 1; i < inputData.length; i++) {
+    arr.push(inputData[i]);
+  }
+  
+  let count = arr.length;
+  let temp = '';
+  
+  arr.forEach((item) => {
+    temp = item[0];
+    for (let i = 1; i < item.length; i++) {
+      if (temp.includes(item[i]) && item[i - 1] !== item[i]) {
+        count--;
+        break;
+      }
+      temp += item[i];
     }
-}
-console.log(count)
+  });
+  
+  console.log(count);

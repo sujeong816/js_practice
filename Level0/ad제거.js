24d27b5bd1dbdee2fe9886e158eb9ab18728ae4f
sujeong816 <sujeong1517@naver.com>
2023-04-29 23:14:16 +0900

    코딩 기초 트레이닝 ad 제거하기

---
+++ Level0/ad제거.js
@@ -0,0 +1,11 @@
+function solution(strArr) {
+    var answer = [];
+    for(let i=0; i<strArr.length; i++) {
+        if(!strArr[i].includes('ad')) {
+            answer.push(strArr[i])
+        }
+    }
+    return answer;
+}
+
+solution

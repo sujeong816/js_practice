014b9ef4835b19e0ae7132cdc320f140eab84c54
sujeong816 <sujeong1517@naver.com>
2023-05-25 23:31:00 +0900

    수정

---
+++ Level1/test.js
@@ -0,0 +1,13 @@
+let all = 0;
+let multiple=0;
+
+for (let i = 1; i <= 55; i++) {
+    all += i;
+}
+console.log(all);
+
+
+for (let i = 5; i <= 55; i += 5) {
+  multiple += i;
+}
+console.log(multiple);

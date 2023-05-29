52d423205f3b5774b1e96ce940c6fee106046069
sujeong816 <sujeong1517@naver.com>
2023-05-29 21:12:06 +0900

    A+B -2

---
+++ backjoon/b5/2558.js
@@ -0,0 +1,6 @@
+import fs from "fs";
+//const fs = require('fs');
+let inputData = fs.readFileSync("./index.txt").toString().trim().split("\n");
+// let inputData = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+console.log(+inputData[0]+(+inputData[1]))

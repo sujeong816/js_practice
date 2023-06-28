c37d12f0d291713db3133be4083f404bf00754c3
sujeong816 <sujeong1517@naver.com>
2023-06-28 23:23:58 +0900

    좌표 정렬하기

---
+++ backjoon/s5/11650.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+//const fs = require('fs');
+let data = fs.readFileSync("./index.txt").toString().trim().split("\n").map(i=>i.split(" ").map(Number));
+// let data = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(i=>i.split(" ").map(Number));
+
+data.shift()
+data.sort((i,j)=>(i[0] == j[0] ? i[1] - j[1] : i[0] - j[0]))
+
+let str = ''
+for(let i=0; i<data.length; i++) {
+    for(let j=0; j<data[i].length; j++) {
+        str += data[i][j]+' '
+    }
+    str += '\n'
+}
+console.log(str)

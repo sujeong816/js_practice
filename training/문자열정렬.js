2c1fc88317d9f668df3f231bcd186a75dc10668f
sujeong816 <sujeong1517@naver.com>
2023-07-18 15:51:42 +0900

    문자열 잘라서 정렬하기

---
+++ training/문자열정렬.js
@@ -0,0 +1,9 @@
+function solution(myString) {
+    myString = myString.split('')
+    
+    for(let i=0; i<myString.length; i++) {
+        if(myString[i] == 'x') { myString[i] = ' '}
+    }
+    
+    return myString.join('').trim().split(' ').sort()
+}

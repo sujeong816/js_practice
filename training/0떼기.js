c632428d18bbd1ce51daa7d9626833a09859bfa3
sujeong816 <sujeong1517@naver.com>
2023-07-18 14:57:58 +0900

    0 떼기

---
+++ training/0떼기.js
@@ -0,0 +1,8 @@
+function solution(n_str) {
+    // n_str = n_str.split('')
+    // for(let i=0; i<n_str.length; i++) {
+    //     if(n_str[i] !== '0')
+    //         return n_str.slice(i).join('')
+    // }
+    return String(Number(n_str))
+}

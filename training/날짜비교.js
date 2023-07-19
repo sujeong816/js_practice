d76575211ba3f1d860292f7bcbcf13c6155d3ce4
sujeong816 <sujeong1517@naver.com>
2023-07-20 00:19:33 +0900

    M: modify 날짜 비교하기

--- training/날짜비교.js
+++ training/날짜비교.js
@@ -1,12 +1,5 @@
 function solution(date1, date2) {
-    if(date1[0] < date2[0]) { return 1 } //연도가 앞설 때
-    else if(date1[0] > date2[0]) { return 0 }
-    else {
-        if(date1[1] < date2[1]) { return 1 } //월이 앞설 때
-        else if(date1[1] > date2[1]) { return 0 }
-        else { //일이 앞설 때
-            if(date1[2] < date2[2]) { return 1 }
-            else { return 0 }
-        }
-    }
+    let d1 = new Date(date1)
+    let d2 = new Date(date2)
+    return d1 < d2 ? 1 : 0
 }

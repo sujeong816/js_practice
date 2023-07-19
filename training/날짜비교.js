86a0068f804d990b43feff69e7f0dc3f132a1fc0
sujeong816 <sujeong1517@naver.com>
2023-07-20 00:09:25 +0900

    날짜 비교하기

---
+++ training/날짜비교.js
@@ -0,0 +1,12 @@
+function solution(date1, date2) {
+    if(date1[0] < date2[0]) { return 1 } //연도가 앞설 때
+    else if(date1[0] > date2[0]) { return 0 }
+    else {
+        if(date1[1] < date2[1]) { return 1 } //월이 앞설 때
+        else if(date1[1] > date2[1]) { return 0 }
+        else { //일이 앞설 때
+            if(date1[2] < date2[2]) { return 1 }
+            else { return 0 }
+        }
+    }
+}

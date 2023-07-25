4373cf5f0e9ed2e0db836cffb585b5cdebbd2fac
sujeong816 <sujeong1517@naver.com>
2023-07-25 17:03:22 +0900

    수열과 쿼리 4

--- training/수열쿼리4.js
+++ training/수열쿼리4.js
@@ -2,10 +2,14 @@ function solution(arr, q) {
     //[s,e,k] j가 k 배수면 arr[i]1 더함 i는 s이상 e이하
     
     for(let i=0; i<q.length; i++) {
-        for(let j=q[0]; j<=q[1]; j++) {
-            if(j%q[2] == 0) { arr[j]+=1 }
+        let s = q[i][0]; let e = q[i][1]; let k = q[i][2]
+
+        for(let j=s; j<=e; j++) {
+            if(j%k == 0) { arr[j]+=1 }
         }
     }
     
-    return arr
+    console.log(arr)
 }
+
+solution([0, 1, 2, 4, 3], [[0, 4, 1],[0, 3, 2],[0, 3, 3]])

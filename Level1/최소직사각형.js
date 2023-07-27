e8427c39a4486d446dab9838cbb9ebbd97c0f06e
sujeong816 <sujeong1517@naver.com>
2023-07-27 23:52:18 +0900

    M: modify 리팩토링 최소직사각형.js

--- Level1/최소직사각형.js
+++ Level1/최소직사각형.js
@@ -1,14 +1,26 @@
+// function solution(sizes) {
+//     let left = []; let right = [];
+//     for(let i=0; i<sizes.length; i++) {
+//         if(sizes[i][0] > sizes[i][1]){
+//             left.push(sizes[i][0])
+//             right.push(sizes[i][1])
+//         }else {
+//             left.push(sizes[i][1])
+//             right.push(sizes[i][0])
+//         }
+//     }
+//     return Math.max(...left)*Math.max(...right);
+// }
+// solution([[60, 50], [30, 70], [60, 30], [80, 40]])
 function solution(sizes) {
-    let left = []; let right = [];
+    let left = []; let right = []
+    
     for(let i=0; i<sizes.length; i++) {
-        if(sizes[i][0] > sizes[i][1]){
-            left.push(sizes[i][0])
-            right.push(sizes[i][1])
-        }else {
-            left.push(sizes[i][1])
-            right.push(sizes[i][0])
-        }
+        let l = sizes[i][0]; let r = sizes[i][1]
+        
+        if(l>r) { left.push(l); right.push(r) }
+        else { left.push(r); right.push(l) }
     }
-    return Math.max(...left)*Math.max(...right);
+    
+    return Math.max(...left) * Math.max(...right)
 }
-solution([[60, 50], [30, 70], [60, 30], [80, 40]])

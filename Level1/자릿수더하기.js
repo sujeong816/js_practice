7a260f90557070d8851ba41bf20b72e593a1bf97
sujeong816 <sujeong1517@naver.com>
2023-08-08 23:29:53 +0900

    자릿수 더하기 리팩토링

--- Level1/자릿수더하기.js
+++ Level1/자릿수더하기.js
@@ -1,4 +1,3 @@
-function solution(n){ 
-    if(n<10) { return n; }
-    else {return n.toString().split("").reduce((a,b)=>Number(a)+Number(b));}
+function solution(n){
+    return n.toString().split('').map(i=>Number(i)).reduce((i,j)=>i+j)
 }

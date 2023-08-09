bdde536a44f43cce1d438c4eb03476e7256308ff
sujeong816 <sujeong1517@naver.com>
2023-08-09 23:03:34 +0900

    제일 작은 수 제거하기 리팩토링

--- Level1/min.js
+++ Level1/min.js
@@ -1,7 +1,4 @@
 function solution(arr) {
-    if(arr.length==1) { return [-1]; }
-    else { 
-        arr.splice(arr.indexOf(Math.min(...arr)), 1);
-        return arr;
-    }
+    arr.splice(arr.indexOf(Math.min(...arr)),1)
+    return arr.length ? arr : [-1]
 }

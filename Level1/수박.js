39bd3ffab6ed2d551275547cee82065ed2e6b463
sujeong816 <sujeong1517@naver.com>
2023-08-09 23:48:44 +0900

    수박수박수박수박수박수? 리팩토링

--- Level1/수박.js
+++ Level1/수박.js
@@ -1,6 +1,9 @@
+// function solution(n) {
+//     var answer = '';
+//     for(let i=0; i<parseInt(n/2); i++) { answer += '수박'; }
+//     if(n%2==1) { answer += '수'; }
+//     return answer;
+// }
 function solution(n) {
-    var answer = '';
-    for(let i=0; i<parseInt(n/2); i++) { answer += '수박'; }
-    if(n%2==1) { answer += '수'; }
-    return answer;
+    return n%2==0 ? '수박'.repeat(n/2) : '수박'.repeat(Math.floor(n/2))+'수'
 }

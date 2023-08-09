447224ea4bee242836e6e27fb1978f1ad2907a2c
sujeong816 <sujeong1517@naver.com>
2023-08-10 00:22:10 +0900

    약수의 개수와 덧셈 리팩토링

--- Level1/약수덧셈.js
+++ Level1/약수덧셈.js
@@ -1,14 +1,13 @@
 function solution(left, right) {
-    var answer = 0; let n = 0;
+    let answer = 0
+    
     for(let i=left; i<=right; i++) {
+        let count = 0
         for(let j=1; j<=i; j++) {
-            if(i%j==0) { n+=1; }
+            if(i%j==0) count++
         }
-        console.log(i);
-        console.log(n);
-        n%2===0 ? answer += i : answer -= i;
-        n = 0;
+        count%2==0 ? answer += i : answer -= i
     }
-    return answer;
+    return answer
 }
 solution(13, 17);

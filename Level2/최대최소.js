631a0a08a3e87826ee440c19d4203f0948dee0ff
sujeong816 <sujeong1517@naver.com>
2023-08-14 02:34:51 +0900

    최댓값과 최솟값

--- Level2/최대최소.js
+++ Level2/최대최소.js
@@ -1,4 +1,9 @@
+// function solution(s) {
+//     let arr = s.split(" ").map(i=>parseInt(i)).sort((i,j) => i-j);
+//     return arr[0]+" "+arr[arr.length-1];
+// }
+
 function solution(s) {
-    let arr = s.split(" ").map(i=>parseInt(i)).sort((i,j) => i-j);
-    return arr[0]+" "+arr[arr.length-1];
+    s = s.split(' ')
+    return Math.min(...s)+' '+Math.max(...s)
 }
